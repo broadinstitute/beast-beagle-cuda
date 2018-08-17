fd9c331f59fd2d4e1a992fb2ee04f682acf8ec1b
Danny Park <dpark@broadinstitute.org>
2018-08-16 23:43:08 -0400

    update beagle and beast

--- install-beagle.sh
+++ install-beagle.sh
@@ -4,8 +4,8 @@ cd /opt/docker
 
 git clone https://github.com/beagle-dev/beagle-lib.git
 cd beagle-lib
-# Nov 2017. Future commits break things?
-git checkout bcd2bf1a0b17703e8e24d56e0f1b5b4967470b8b
+## Nov 2017. Future commits break things?
+#git checkout bcd2bf1a0b17703e8e24d56e0f1b5b4967470b8b
 
 ./autogen.sh
 ./configure --prefix=/usr/local
--- install-beast.sh
+++ install-beast.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-#wget --quiet https://github.com/beast-dev/beast-mcmc/releases/download/v1.8.4/BEASTv1.8.4.tgz
-#tar -xzpf BEASTv1.8.4.tgz
-#rm BEASTv1.8.4.tgz
+## the section below pulls a named version
+
+wget --quiet https://github.com/beast-dev/beast-mcmc/releases/download/v1.10.1/BEASTv1.10.1.tgz
+tar -xzpf BEASTv1.10.1.tgz
+rm BEASTv1.10.1.tgz
+
+mv BEASTv1.10.1/bin/* /usr/local/bin
+mv BEASTv1.10.1/lib/* /usr/local/lib
+
+## the section below pulls the latest HEAD from master
 #
-#mv BEASTv1.8.4/bin/* /usr/local/bin
-#mv BEASTv1.8.4/lib/* /usr/local/lib
+#cd /opt/docker
+#
+#git clone https://github.com/beast-dev/beast-mcmc.git
+#cd beast-mcmc
+#
+#ant dist
+#
+#mv build/dist/* /usr/local/lib
+#mv release/Linux/lib/* /usr/local/lib
+#mv release/Linux/scripts/* /usr/local/bin
 
-cd /opt/docker
-
-git clone https://github.com/beast-dev/beast-mcmc.git
-cd beast-mcmc
-
-ant dist
-
-mv build/dist/* /usr/local/lib
-mv release/Linux/lib/* /usr/local/lib
-mv release/Linux/scripts/* /usr/local/bin
-
+## however it got installed, let's test that it works:
 beast -beagle_info
