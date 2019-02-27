3562439de3d901457bcc0ddc37471b86fcc57fe5
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2019-02-27 01:15:23 -0500

    in run_beast.sh, parameterize BEAGLE_ORDER, add guardrails to enforce INPUT_FILE is set

--- run_beast.sh
+++ run_beast.sh
@@ -6,10 +6,19 @@
 IN_DIR=$(dirname "${INPUT_FILE}")
 OUT_DIR=$(dirname "${OUTPUT_FILES}")
 OUTPUT_PREFIX=$(basename "${INPUT_FILE}" .xml)
+if [ -z "${BEAGLE_ORDER}" ]; then
+  BEAGLE_ORDER=1 # run on first GPU only if BEAGLE_ORDER is not set
+fi
+
+if [ -z "${INPUT_FILE}" ]; then
+    echo "Usage: $(basename $0) [beagle_order]"
+    echo '       The input xml must be passed via INPUT_FILE=/path/to/beauti_generated_input.xml'
+    exit 1
+fi
 
 pwd 
 cd $OUT_DIR
 beast -beagle_info > "${OUTPUT_PREFIX}.out"
 pwd 
-beast -beagle_GPU -beagle_cuda -beagle_double -beagle_scaling always -beagle_order 1 ${INPUT_FILE} >> "${OUTPUT_PREFIX}.out"
+beast -beagle_GPU -beagle_cuda -beagle_double -beagle_scaling always -beagle_order ${BEAGLE_ORDER} ${INPUT_FILE} >> "${OUTPUT_PREFIX}.out"
 ls 
