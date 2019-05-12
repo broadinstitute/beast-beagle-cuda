139286757c0c81eaa1940aadbc2d70de927569b1
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2019-05-12 14:48:16 -0400

    allow docker image tag to be set
    allow docker image tag to be set, so images with different versions of BEAST can be specified

--- dsub_beast.sh
+++ dsub_beast.sh
@@ -9,7 +9,18 @@
 # 
 
 function print_usage(){
-    echo "Usage: $(basename $0) gs://path/to/in.xml gcp-project-name num_gpus [beagle_order]"
+  echo "Usage: "
+  echo "  $(basename $0) gs://path/to/in.xml gcp-project-name num_gpus [beagle_order]"
+  echo ""
+  echo "  Note: The version of BEAST used should match the version of BEAUTi"
+  echo "        used to generate the input xml file."
+  echo ""
+  echo "        Docker images have been built for several versions of BEAST."
+  echo "        The Docker image to be used can be selected by environment variable. For example:"
+  echo "          BEAST_VERSION='1.10.4' $(basename $0) gs://path/to/in.xml gcp-project-name num_gpus [beagle_order]"
+  echo "        For available versions of BEAST, see the tags on Quay.io:"
+  echo "          https://quay.io/repository/broadinstitute/beast-beagle-cuda?tab=tags"
+  echo "        If BEAST_VERSION is not specified the 'latest' tag will be used."
 }
 
 if [ $# -eq 0 ] || [ $# -lt 3 ]; then
@@ -17,10 +28,36 @@ if [ $# -eq 0 ] || [ $# -lt 3 ]; then
     exit 1
 fi
 
+function absolute_path() {
+    local SOURCE="$1"
+    while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+        DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+        if [[ "$OSTYPE" == "darwin"* ]]; then
+            SOURCE="$(readlink "$SOURCE")"
+        else
+            SOURCE="$(readlink -f "$SOURCE")"
+        fi
+        [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    done
+    echo "$SOURCE"
+}
+SOURCE="${BASH_SOURCE[0]}"
+SCRIPT=$(absolute_path "$SOURCE")
+SCRIPT_DIRNAME="$(dirname "$SOURCE")"
+SCRIPTPATH="$(cd -P "$(echo $SCRIPT_DIRNAME)" &> /dev/null && pwd)"
+SCRIPT="$SCRIPTPATH/$(basename "$SCRIPT")"
+
+DOCKER_IMAGE="quay.io/broadinstitute/beast-beagle-cuda"
+if [ -z "${BEAST_VERSION}" ]; then
+  DOCKER_IMAGE_TAG=":latest"
+else
+  DOCKER_IMAGE_TAG=":${BEAST_VERSION}"
+fi
+
 IN_XML="$1"
 OUT_BUCKET="$(dirname $1)"
 GCP_PROJECT="$2"
-DOCKER_IMAGE="quay.io/broadinstitute/beast-beagle-cuda"
+
 NUM_GPUS="$3"
 GPU_TYPE="nvidia-tesla-k80" # see: https://cloud.google.com/compute/docs/gpus/
 if [ -z "$4" ]; then
@@ -36,11 +73,11 @@ dsub \
   --accelerator-type "${GPU_TYPE}" \
   --nvidia-driver-version "396.37" \
   --accelerator-count "${NUM_GPUS}" \
-  --image "${DOCKER_IMAGE}" \
+  --image "${DOCKER_IMAGE}${DOCKER_IMAGE_TAG}" \
   --input "INPUT_FILE=${IN_XML}" \
   --output "OUTPUT_FILES=${OUT_BUCKET}/*" \
   --logging "${OUT_BUCKET}" \
   --env BEAGLE_ORDER="${BEAGLE_ORDER}"\
-  --script "run_beast.sh"  \
+  --script "${SCRIPTPATH}/run_beast.sh"  \
   --boot-disk-size 15 \
   #--wait
--- install-beast.sh
+++ install-beast.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-wget --quiet https://github.com/beast-dev/beast-mcmc/releases/download/v1.10.4/BEASTv1.10.4.tgz
-tar -xzpf BEASTv1.10.4.tgz
-rm BEASTv1.10.4.tgz
+beast_version="1.10.4"
 
-mv BEASTv1.10.4/bin/* /usr/local/bin
-mv BEASTv1.10.4/lib/* /usr/local/lib
+wget --quiet https://github.com/beast-dev/beast-mcmc/releases/download/v${beast_version}/BEASTv${beast_version}.tgz
+tar -xzpf BEASTv${beast_version}.tgz
+rm BEASTv${beast_version}.tgz
+
+mv BEASTv${beast_version}/bin/* /usr/local/bin
+mv BEASTv${beast_version}/lib/* /usr/local/lib
 
 beast -beagle_info
