4c6a862d6e596cde8b74f93ef10581691728c58a
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2020-06-10 17:54:30 -0400

    account for different pre-release package URL

--- install-beast.sh
+++ install-beast.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 beast_version="1.10.5pre"
+# uncomment when out of pre-release:
+#wget --quiet https://github.com/beast-dev/beast-mcmc/releases/download/v${beast_version}/BEASTv${beast_version}.tgz
+wget --quiet https://github.com/beast-dev/beast-mcmc/releases/download/v1.10.5pre1/BEASTv1.10.5pre.tgz -O BEASTv${beast_version}.tgz
 
-wget --quiet https://github.com/beast-dev/beast-mcmc/releases/download/v${beast_version}/BEASTv${beast_version}.tgz
 tar -xzpf BEASTv${beast_version}.tgz
 rm BEASTv${beast_version}.tgz
 
