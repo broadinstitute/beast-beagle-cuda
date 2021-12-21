5595efdb86df1c0c3ab6432d04a646bc6dfa8d72
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2021-12-20 19:52:26 -0500

    do not pin google-cloud-sdk to specific debian version (since "focal" build for 20.04 is not in apt repo)

--- install-apt_packages.sh
+++ install-apt_packages.sh
@@ -35,7 +35,11 @@ DEBIAN_PLATFORM="$(lsb_release -c -s)"
 echo "Debian platform: $DEBIAN_PLATFORM"
 
 # Add source for gcloud sdk
-echo "deb http://packages.cloud.google.com/apt cloud-sdk-$DEBIAN_PLATFORM main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+echo "deb http://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+# The line below is commented out since there is not a "focal" build of cloud-sdk in the apt repo (20.04); 
+#   remove the line above and uncomment below when available.
+# echo "deb http://packages.cloud.google.com/apt cloud-sdk-$DEBIAN_PLATFORM main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
 # Install gcloud and aws
