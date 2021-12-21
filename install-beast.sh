0ac969db00fb197382219ea8e875564be7553e3b
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2021-12-21 00:21:30 -0500

    adjust beast version string

--- install-beast.sh
+++ install-beast.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-beast_version="1.10.5pre_thorney_v0.1.1"
+beast_version="1.10.5pre_thorney_0.1.1"
 # uncomment when out of pre-release:
 #wget --quiet https://github.com/beast-dev/beast-mcmc/releases/download/v${beast_version}/BEASTv${beast_version}.tgz
 wget --quiet https://github.com/beast-dev/beast-mcmc/releases/download/v1.10.5pre_thorney_v0.1.1/BEASTv1.10.5pre_thorney_0.1.1.tgz -O BEASTv${beast_version}.tgz
