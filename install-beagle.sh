ca9ad4a981d0a2df0d969ee167b9dd0287160e50
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2022-01-06 12:30:48 -0500

    report cpu info before beagle build, and set BEAGLE_OPTIMIZE_FOR_NATIVE_ARCH=false
    report cpu info before beagle build, and set BEAGLE_OPTIMIZE_FOR_NATIVE_ARCH=false so hopefully the binary is compatible with both the quay build host and the PAPI COS host

--- install-beagle.sh
+++ install-beagle.sh
@@ -2,6 +2,9 @@
 
 set -e -o pipefail
 
+# report CPU info
+lscpu
+
 cd /opt/docker
 
 # beagle 3.1.2, known working with beast 1.10.5pre
@@ -13,7 +16,7 @@ git checkout aee7aae
 
 mkdir build
 cd build
-cmake -DBUILD_OPENCL=OFF -DCMAKE_INSTALL_PREFIX:PATH=/usr/local ..
+cmake -DBUILD_OPENCL=OFF -BEAGLE_OPTIMIZE_FOR_NATIVE_ARCH=false -DCMAKE_INSTALL_PREFIX:PATH=/usr/local ..
 make install
 
 ldconfig # LD_LIBRARY_PATH is also set in the Dockerfile to include /usr/local/lib
