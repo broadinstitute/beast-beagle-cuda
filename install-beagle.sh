49e55c96382258d2680a94fd9d43294cce5575c0
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2022-01-06 14:38:39 -0500

    pin to v3.1.2; add missing 'D' to cmake arg

--- install-beagle.sh
+++ install-beagle.sh
@@ -10,13 +10,13 @@ cd /opt/docker
 # beagle 3.1.2, known working with beast 1.10.5pre
 #git clone --depth=1 --branch="v3.1.2" https://github.com/beagle-dev/beagle-lib.git
 # localize beagle pinned to commit aee7aae with changes since v3.1.2 release
-git clone --depth=1 https://github.com/beagle-dev/beagle-lib.git
+git clone --depth=1 --branch="v3.1.2" https://github.com/beagle-dev/beagle-lib.git
 cd beagle-lib
 git checkout aee7aae
 
 mkdir build
 cd build
-cmake -DBUILD_OPENCL=OFF -BEAGLE_OPTIMIZE_FOR_NATIVE_ARCH=false -DCMAKE_INSTALL_PREFIX:PATH=/usr/local ..
+cmake -DBUILD_OPENCL=OFF -DBEAGLE_OPTIMIZE_FOR_NATIVE_ARCH=false -DCMAKE_INSTALL_PREFIX:PATH=/usr/local ..
 make install
 
 ldconfig # LD_LIBRARY_PATH is also set in the Dockerfile to include /usr/local/lib
