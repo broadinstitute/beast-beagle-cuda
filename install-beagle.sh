b004ce74effc4492d8a28d13362c981b2dc4efb6
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2022-12-14 15:41:40 -0500

    clone beagle v4.0.0

--- install-beagle.sh
+++ install-beagle.sh
@@ -10,10 +10,8 @@ cd /opt/docker
 
 # beagle 3.1.2, known working with beast 1.10.5pre
 #git clone --depth=1 --branch="v3.1.2" https://github.com/beagle-dev/beagle-lib.git
-# localize beagle pinned to commit aee7aae with changes since v3.1.2 release
-git clone --depth=1 https://github.com/beagle-dev/beagle-lib.git
+git clone --depth=1 --branch "v4.0.0" https://github.com/beagle-dev/beagle-lib.git
 cd beagle-lib
-git checkout aee7aae
 
 mkdir build
 cd build
