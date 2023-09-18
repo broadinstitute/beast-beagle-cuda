d741156f0811709e96124a450c5fb7a86538e054
Daniel Park <dpark@broadinstitute.org>
2023-09-18 09:18:45 -0400

    pull hmc-clock branch of beast-mcmc

--- install-beast.sh
+++ install-beast.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-beast_version="1.10.5pre_thorney_0.1.2"
-# uncomment when out of pre-release:
-#wget --quiet https://github.com/beast-dev/beast-mcmc/releases/download/v${beast_version}/BEASTv${beast_version}.tgz
-wget --quiet https://github.com/beast-dev/beast-mcmc/releases/download/v1.10.5pre_thorney_v0.1.2/BEASTv1.10.5pre_thorney_0.1.2.tgz -O BEASTv${beast_version}.tgz
+#### normal stuff here
+#beast_version="1.10.5pre_thorney_0.1.2"
+## uncomment when out of pre-release:
+##wget --quiet https://github.com/beast-dev/beast-mcmc/releases/download/v${beast_version}/BEASTv${beast_version}.tgz
+#wget --quiet https://github.com/beast-dev/beast-mcmc/releases/download/v1.10.5pre_thorney_v0.1.2/BEASTv1.10.5pre_thorney_0.1.2.tgz -O BEASTv${beast_version}.tgz
+#
+#tar -xzpf BEASTv${beast_version}.tgz
+#rm BEASTv${beast_version}.tgz
+#
+#mv BEASTv${beast_version}/bin/* /usr/local/bin
+#mv BEASTv${beast_version}/lib/* /usr/local/lib
 
-tar -xzpf BEASTv${beast_version}.tgz
-rm BEASTv${beast_version}.tgz
 
-mv BEASTv${beast_version}/bin/* /usr/local/bin
-mv BEASTv${beast_version}/lib/* /usr/local/lib
+#### pull dev hotfix version from Marc
+git clone --depth=1 --branch hmc-clock https://github.com/beast-dev/beast-mcmc.git
+cd beast-mcmc
+ant dist
+mv bin/* /usr/local/bin
+mv lib/* /usr/local/lib
 
+#### test
 beast -beagle_info
